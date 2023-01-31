e035b63613e55e800737e7ce6616a98f83045f05
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2023-01-31 18:31:09 +0200

    Photo Upload

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -46,7 +46,9 @@ namespace API.Controllers
         [HttpPost("login")] // POST: api/account/login
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("invalid username");
 
@@ -62,8 +64,9 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
-            };;
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            };
         }
 
         private async Task<bool> UserExists(string username)
